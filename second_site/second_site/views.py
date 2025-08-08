195fee1fd6dcaad1252b6fbf6b0770ca64e9332e
rscortgamerYT <royalshikher@gmail.com>
2025-08-08 08:29:14 +0530

    first commit

---
+++ second_site/second_site/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index (request):
+    return HttpResponse("Home Page")
+
+def home(request):
+    return render(request, 'index.html')
